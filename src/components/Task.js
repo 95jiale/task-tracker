1b6abfd7602db1de9de0dc84bec318f66c1154d1
Jiale <95jiale@gmail.com>
2022-02-25 01:16:57 +0800

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,7 @@ const Task = ({task}) => {
   return (
     <div className ='task'>
         <h3>
-            {task.text} <FaTimes />
+            {task.text} <FaTimes style ={{color : 'red' , cursor : 'pointer'}} /> 
         </h3>
         <p>{task.day}</p>
     </div>
