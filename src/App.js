b27d7115344ad52ffc249babc012ebaaedddc05c
Jiale <95jiale@gmail.com>
2022-02-25 20:39:24 +0800

    add task and toggle buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 
 
-function App() {    
+function App() {  
+  const [showAddTask,setShowAddTask]  = useState (false)
   const [tasks, setTasks] = useState([
   {
       id : 1,
@@ -39,16 +41,29 @@ function App() {
   },
 ]
 )
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() *10000)+1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
   //Delete task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task)=>task.id !== id))
+    setTasks(tasks.filter((task)=>task.id !== id)) //ask jh
+  }
+  // Toggle remind
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id === id ? {...task, reminder : !task.reminder} : task)) //ask jh
   }
 
   return (
     <div className="container">
-    <Header />
-    {tasks.length > 0 ?(<Tasks tasks ={tasks} onDelete={deleteTask}/>) : 
+    <Header onAdd = {() => setShowAddTask
+    (!showAddTask)} showAdd = {showAddTask}
+    />
+    {showAddTask && <AddTask onAdd ={addTask} />}
+    {tasks.length > 0 ?(<Tasks tasks ={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : 
     ('No task to show')
     }
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log ('click')
-    }
+const Header = ({title , onAdd, showAdd}) => {
+
   return (
     <header className ='header'> 
         <h1>{title}</h1>
-        <Button color='green' text ='Add' onClick ={onClick} />
-    </header>
+        <Button color='green' text ={showAdd ? 'Close' : 'Add'} onClick ={onAdd} /> 
+    </header> //ask jh
   )
 }
 
