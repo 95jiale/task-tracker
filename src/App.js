81dea4eb76375d4fde00aecbeb0ee5bde345aa11
Jiale <95jiale@gmail.com>
2022-02-25 16:21:29 +0800

    Delete tasks update

--- src/App.js
+++ src/App.js
@@ -42,13 +42,15 @@ function App() {
 
   //Delete task
   const deleteTask = (id) => {
-    console.log ('delete',id)
+    setTasks(tasks.filter((task)=>task.id !== id))
   }
 
   return (
     <div className="container">
     <Header />
-    <Tasks tasks ={tasks}/>
+    {tasks.length > 0 ?(<Tasks tasks ={tasks} onDelete={deleteTask}/>) : 
+    ('No task to show')
+    }
     </div>
   );
 }
